b84fb3bc38221eafa10c5aede4e948583d29ca0e
David Piauilino <david.piauilino@yduqs.com.br>
2024-05-06 14:54:34 -0300

    atividade semana 8

--- src/test/usuario.spec.ts
+++ src/test/usuario.spec.ts
@@ -2,9 +2,27 @@ import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from '../app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 describe('UsuarioController', () => {
   let app;
+
+  const novoUsuario = {
+    nome: 'Matheus',
+    email: 'matheus@usuario.com',
+    senha: 'Teste-123',
+    cpf: '09245326705',
+    telefone: '21983578705',
+    enderecos: [{
+      rua: 'Rua Sgt Joao',
+      numero: '22',
+      complemento: 'sem complemento',
+      bairro: 'Sao Vicente',
+      cidade: 'Belford Roxo',
+      estado: 'Rio de Janeiro',
+      cep: '26178360'
+    }]
+  };
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,14 +31,45 @@ describe('UsuarioController', () => {
 
     app = moduleFixture.createNestApplication();
     app.useGlobalPipes(new ValidationPipe());
+    useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
     await app.init();
   });
 
   it('deve inserir um novo usuário', async () => {
-    const novoUsuario = {
+
+    const response = await request(app.getHttpServer())
+      .post('/usuario')
+      .send(novoUsuario);
+
+    expect(response.status).toBe(201);
+    expect(response.body.usuario).toHaveProperty('id');
+    expect(response.body.usuario.nome).toBe(novoUsuario.nome);
+    expect(response.body.usuario.email).toBe(novoUsuario.email);
+  });
+
+  it('deve listar todos os usuários', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/usuario');
+
+    expect(response.status).toBe(200);
+    expect(response.body.usuarios).toBeInstanceOf(Array);
+  });
+
+  it('deve não adicionar um usuário com email já existente', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/usuario')
+      .send(novoUsuario);
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toContain("Já existe um usuário com este e-mail");
+  });
+
+  it('deve não adicionar um usuário com email inválido', async () => {
+
+    const emailInvalido = {
       nome: 'Matheus',
-      email: 'matheus@usuario.com',
+      email: 'matheususuario.com',
       senha: 'Teste-123',
       cpf: '09245326705',
       telefone: '21983578705',
@@ -34,15 +83,12 @@ describe('UsuarioController', () => {
         cep: '26178360'
       }]
     };
-
     const response = await request(app.getHttpServer())
       .post('/usuario')
-      .send(novoUsuario);
+      .send(emailInvalido);
 
-    expect(response.status).toBe(201);
-    expect(response.body.usuario).toHaveProperty('id');
-    expect(response.body.usuario.nome).toBe(novoUsuario.nome);
-    expect(response.body.usuario.email).toBe(novoUsuario.email);
+    expect(response.status).toBe(400);
+    expect(response.body.message).toContain("O e-mail informado é inválido");
   });
 
   afterAll(async () => {
